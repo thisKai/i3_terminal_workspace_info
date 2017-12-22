240c827ef11c8caecca149a05d2b33a5e7ffe84a
Kai Patrick <thiskaiguy@gmail.com>
2017-12-22 13:20:39 +0000

    Run a stub function on every workspace event

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,26 @@
 extern crate i3ipc;
 
+use i3ipc::{
+    I3Connection,
+    I3EventListener,
+    Subscription,
+};
+
+fn debug_workspaces(connection: &mut I3Connection) -> Result<(), ::i3ipc::MessageError> {
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut connection = I3Connection::connect().unwrap();
+
+    let _ = debug_workspaces(&mut connection);
+
+    let mut listener = I3EventListener::connect().unwrap();
+
+    let subs = [Subscription::Workspace];
+    listener.subscribe(&subs).unwrap();
+
+    for _event in listener.listen() {
+        let _ = debug_workspaces(&mut connection);
+    }
 }
