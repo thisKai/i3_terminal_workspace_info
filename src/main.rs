5b9ebb77f68131f37cb1cc31daa215da15749408
Kai Patrick <thiskaiguy@gmail.com>
2017-12-22 13:25:02 +0000

    Output the displays and workspaces on workspace events
    Show which workspace is active

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,38 @@
+#![feature(use_nested_groups)]
 extern crate i3ipc;
+
+use std::io::{self, Write};
 
 use i3ipc::{
     I3Connection,
     I3EventListener,
     Subscription,
+    reply::{
+        Outputs,
+        Output,
+        Workspaces,
+        Workspace,
+    }
 };
 
 fn debug_workspaces(connection: &mut I3Connection) -> Result<(), ::i3ipc::MessageError> {
+    let &Outputs { ref outputs } = &connection.get_outputs().unwrap();
+    let screens = outputs.iter().filter(|output| output.current_workspace.is_some());
+    let Workspaces { ref workspaces } = connection.get_workspaces()?;
+    for &Output { ref name, .. } in screens {
+        print!("[ {}: ", name);
+        let workspaces_on_this_screen = workspaces.iter().filter(|ws| &ws.output == name);
+        for &Workspace { ref name, focused, .. } in workspaces_on_this_screen {
+            if focused {
+                print!("<{}>", name);
+            } else {
+                print!(" {} ", name);
+            }
+        }
+        print!(" ] ");
+    }
+    println!();
+    let _ = io::stdout().flush();
     Ok(())
 }
 
